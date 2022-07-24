9bc499d0ea113f198057e2a74e10cdc35d07a080
Margrami <margrami@users.noreply.github.com>
2022-07-24 20:24:42 +0200

    change min variable.

--- timelapse.py
+++ timelapse.py
@@ -7,7 +7,7 @@ from os import system
 import datetime
 from time import sleep
 
-tlminutes = 10 #set this to the number of minutes you wish to run your timelapse camera
+tlminutes = 1 #set this to the number of minutes you wish to run your timelapse camera
 secondsinterval = 10 #number of seconds delay between each photo taken
 fps = 30 #frames per second timelapse video
 numphotos = int((tlminutes*60)/secondsinterval) #number of photos to take
@@ -26,8 +26,8 @@ for i in range(numphotos):
     camera.capture('/home/pi/Pictures/image{0:06d}.jpg'.format(i))
     sleep(secondsinterval)
 print("Done taking photos.")
+camera.close()
 print("Please standby as your timelapse video is created.")
-
 system('ffmpeg -r {} -f image2 -s 1024x768 -nostats -loglevel 0 -pattern_type glob -i "/home/pi/Pictures/*.jpg" -vcodec libx264 -crf 25  -pix_fmt yuv420p /home/pi/Videos/{}.mp4'.format(fps, datetimeformat))
 #system('rm /home/pi/Pictures/*.jpg')
 print('Timelapse video is complete. Video saved as /home/pi/Videos/{}.mp4'.format(datetimeformat))
